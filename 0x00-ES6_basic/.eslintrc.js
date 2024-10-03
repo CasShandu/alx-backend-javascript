84ff0b0bebc4f9303b837ad10c27c446d42b7290
CasShandu <cas.shandu@gmail.com>
2024-10-03 17:48:57 +0000

    TASK0

---
+++ 0x00-ES6_basic/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    // Add any custom rules here
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+  },
+};
+
--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,16 +1,15 @@
 export function taskFirst() {
-  const task = 'I prefer const when I can.'; // Use const here
+  const task = "I prefer const when I can."; // Use const here
   return task;
 }
 
 export function getLast() {
-  return ' is okay';
+  return " is okay";
 }
 
 export function taskNext() {
-  let combination = 'But sometimes let'; // Use let here
+  let combination = "But sometimes let"; // Use let here
   combination += getLast();
 
   return combination;
 }
-
