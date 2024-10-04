e30364a8e44a2022edae15ee80bc400cd376b3d3
CasShandu <cas.shandu@gmail.com>
2024-10-04 08:51:25 +0000

    lint problem fix

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,20 +1,35 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
+    browser: false,
+    es6: true,
+    jest: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended',
+    'airbnb-base',
+    'plugin:jest/all',
   ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: ['jest'],
   rules: {
-    // Add any custom rules here
-    'no-unused-vars': 'warn',
     'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
   },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
-
