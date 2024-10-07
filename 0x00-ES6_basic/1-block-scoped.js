aec2160956d5473b22768411d5600eec80874fa2
root <root@Mpumie-PC.lan>
2024-10-07 15:34:44 +0200

    linting of blank lines

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-
-    return [task, task2]; // no need to change these
+    return [task, task2];
   }
-
+  
   return [task, task2];
 }
