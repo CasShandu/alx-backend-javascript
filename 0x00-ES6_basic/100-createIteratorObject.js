5013a6768e606672376d35fcc96525574b2db5f0
root <root@Mpumie-PC.lan>
2024-10-07 15:48:55 +0200

    function error

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,8 +2,8 @@ export default function createIteratorObject(report) {
   const employees = [];
 
   for (const department in report.allEmployees) {
-    if (report.allEmployees.hasOwnProperty(department)) {
 
+    if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
       employees.push(...report.allEmployees[department]);
     }
   }
