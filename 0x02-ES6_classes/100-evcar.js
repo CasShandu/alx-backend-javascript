4d0534bee2004eacecbbac150524260665a97227
root <root@Mpumie-PC.lan>
2024-10-08 13:11:18 +0200

    class implementation named evcar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,22 +1,45 @@
+/* eslint-disable class-methods-use-this */
+
 import Car from './10-car';
 
-export default class EvCar extends Car {
+/**
+ * Represents an Electric Vehicle Car.
+ * @extends Car
+ */
+export default class EVCar extends Car {
+  /**
+   * Create a new EVCar instance.
+   * @param {string} brand - The brand of the car.
+   * @param {string} motor - The type of motor used in the car.
+   * @param {string} color - The color of the car.
+   * @param {number} range - The range of the car in miles.
+   */
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  set range(value) {
-    this._range = value;
-  }
-
+  /**
+   * Get the range of the car.
+   * @returns {number} The range of the car in miles.
+   */
   get range() {
     return this._range;
   }
 
-  cloneCar() {
-    const Species = super.constructor[Symbol.species];
+  /**
+   * Set the range of the car.
+   * @param {number} value - The new range of the car in miles.
+   */
+  set range(value) {
+    this._range = value;
+  }
 
-    return new Species();
+  /**
+   * Create a clone of the car.
+   * @returns {Car} A new Car instance.
+   */
+  cloneCar() {
+    return new Car();
   }
 }
