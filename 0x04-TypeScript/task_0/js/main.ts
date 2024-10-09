edf2e6005a53d5f4d702818e7de8958080f8635e
root <root@Mpumie-PC.lan>
2024-10-09 15:25:59 +0200

    TYPESCRIPT 0

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -20,3 +20,17 @@ const student2: Student = {
 };
 
 const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement('table');
+
+studentsList.forEach((student) => {
+  const row = table.insertRow();
+  const firstNameCell = row.insertCell(0);
+  const locationCell = row.insertCell(1);
+
+  firstNameCell.textContent = student.firstName;
+  locationCell.textContent = student.location;
+});
+
+document.body.appendChild(table);
+
